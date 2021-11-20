e0b7154816a611cb2c249bcb9780b877fad660d5
KooWbOy <yaroslav220705@mail.ru>
2021-11-20 09:54:38 +0300

    WavesDirectorFix

--- WavesDirector.cs
+++ WavesDirector.cs
@@ -6,16 +6,26 @@ using UnityEngine;
 public class WavesDirector : MonoBehaviour
 {
     public GameObject[] Spawners;
-    private int[,] SpawnersOnWaves;
+
+    private int[,] SpawnersOnWaves = new int[,]
+    {
+        {1, 2, 3, -1, -1},
+        {2, 4, 5, 1, -1},
+        {1, 4, 5, -1, -1},
+        {2, 3, 5, -1, -1},
+        {1, 3, 4, 5, -1},
+        {1, 2, 3, 4, 5}
+    };
+
     public int Wave = 0;
     public int EnemiesOnWave = 5;
     public int EnemiesAmount = 0;
     public float PauseAfterWave = 15f;
     private float timeBeforeNextWave;
-    
+
     void Start()
     {
-        timeBeforeNextWave = PauseAfterWave;
+        // timeBeforeNextWave = PauseAfterWave;
     }
 
     void Update()
@@ -24,24 +34,28 @@ public class WavesDirector : MonoBehaviour
         {
             if (timeBeforeNextWave <= 0)
             {
+                timeBeforeNextWave = PauseAfterWave;
                 if (Wave == 0)
                 {
                     for (int i = 0; i < SpawnersOnWaves.GetLength(Wave); i++)
                     {
-                        Spawners[i].GetComponent<Spawner>().CanSpawn = true;
+                        if (SpawnersOnWaves[Wave, i] != -1)
+                            Spawners[i].GetComponent<Spawner>().CanSpawn = true;
                     }
                 }
                 else if (Wave < SpawnersOnWaves.Length)
                 {
                     for (int i = 0; i < SpawnersOnWaves.GetLength(Wave); i++)
                     {
-                        Spawners[i].GetComponent<Spawner>().CanSpawn = false;
+                        if (SpawnersOnWaves[Wave, i] != -1)
+                            Spawners[i].GetComponent<Spawner>().CanSpawn = false;
                     }
 
                     Wave += 1;
                     for (int i = 0; i < SpawnersOnWaves.GetLength(Wave); i++)
                     {
-                        Spawners[i].GetComponent<Spawner>().CanSpawn = true;
+                        if (SpawnersOnWaves[Wave, i] != -1)
+                            Spawners[i].GetComponent<Spawner>().CanSpawn = true;
                     }
 
                     EnemiesOnWave = Convert.ToInt32(Math.Round(EnemiesOnWave * 1.2));
